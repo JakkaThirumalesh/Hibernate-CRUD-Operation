e3bb99360402474df29518261f9405e6f7a7abbc
JakkaThirumalesh <jakkathirumalesh342@gmail.com>
2025-07-25 14:43:45 +0530

    initial commit

---
+++ src/main/java/com/prog/CRUD_APP/App.java
@@ -0,0 +1,10 @@
+package com.prog.CRUD_APP;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/prog/CRUD_APP/Student.java
@@ -0,0 +1,72 @@
+package com.prog.CRUD_APP;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String address;
+	@Column(name="college_name")
+	private String collegeName;
+	private String email;
+	
+	public Student() {
+		super();
+	}
+	
+	public Student(int id, String name, String address, String collegeName, String email) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.address = address;
+		this.collegeName = collegeName;
+		this.email = email;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCollegeName() {
+		return collegeName;
+	}
+
+	public void setCollegeName(String collegeName) {
+		this.collegeName = collegeName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
