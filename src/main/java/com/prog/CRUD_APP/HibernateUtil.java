34d569bb0af221a45a3fa49adae4701e06d96878
JakkaThirumalesh <143436544+JakkaThirumalesh@users.noreply.github.com>
2025-07-26 01:46:14 +0530

    Update HibernateUtil.java

--- src/main/java/com/prog/CRUD_APP/HibernateUtil.java
+++ src/main/java/com/prog/CRUD_APP/HibernateUtil.java
@@ -18,7 +18,7 @@ public class HibernateUtil {
 			properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
 			properties.put(Environment.URL, "jdbc:mysql://localhost:3306/hibernate_db");
 			properties.put(Environment.USER, "root");
-			properties.put(Environment.PASS, "12345");
+			properties.put(Environment.PASS, "pass");
 			properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
 			properties.put(Environment.HBM2DDL_AUTO, "update");
 			properties.put(Environment.SHOW_SQL, "true");
